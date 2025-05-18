48bbdad68ab7d9f4226cbb3ed73aedb25131343a
rocky2109 <rockys.2109rs@gmail.com>
2025-05-18 22:21:15 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,7 +4,6 @@ from aiohttp import web
 from config import API_ID, API_HASH, BOT_TOKEN
 import sqlite3
 import warnings
-from plugins.bio import handle_join_request  # Ensure this is implemented correctly
 
 warnings.filterwarnings("ignore", message=".*message.forward_date.*")
 
