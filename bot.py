f4e923a00dece88c8c5ec69f87dff8d9e944e5b2
rocky2109 <rockys.2109rs@gmail.com>
2025-05-29 18:07:38 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ from pyrogram import Client
 from aiohttp import web
 
 from config import API_ID, API_HASH, BOT_TOKEN
-from plugins.quote import auto_quote_sender  # 🚀 Import quote task
+
 
 warnings.filterwarnings("ignore", message=".*message.forward_date.*")
 
